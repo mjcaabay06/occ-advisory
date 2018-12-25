7553a7b31caad5d9778e52235fcec37f49647497
marccaabay <marc.caabay@paramount.com.ph>
2018-12-25 23:43:47 +0800

    insert user

--- db/seeds/load_department.seeds.rb
+++ db/seeds/load_department.seeds.rb
@@ -1,22 +1,22 @@
-Status.create!([
-  { description: 'Active' },
-  { description: 'Inactive' },
-])
+# Status.create!([
+#   { description: 'Active' },
+#   { description: 'Inactive' },
+# ])
 
-UserDepartment.create!([
-  { description: 'Admin', status_id: 1, code: 'Admin' },
-  { description: 'Lufthansa Teknik Philippines', status_id: 1, code: 'LTP' },
-  { description: 'Fleet Maintenance Management Division', status_id: 1, code: 'FMD' },
-  { description: 'Revenue Management Department', status_id: 1, code: 'RMD' },
-  { description: 'Cabin Crew Scheduling Department', status_id: 1, code: 'CSD' },
-  { description: 'Central Flight Dispatch', status_id: 1, code: 'CFD' },
-  { description: 'Airport Terminal', status_id: 1, code: 'Airport Terminal' },
-  { description: 'OCC', status_id: 1, code: 'OCC' },
-  { description: 'SP', status_id: 1, code: 'SP' },
-])
+# UserDepartment.create!([
+#   { description: 'Admin', status_id: 1, code: 'Admin' },
+#   { description: 'Lufthansa Teknik Philippines', status_id: 1, code: 'LTP' },
+#   { description: 'Fleet Maintenance Management Division', status_id: 1, code: 'FMD' },
+#   { description: 'Revenue Management Department', status_id: 1, code: 'RMD' },
+#   { description: 'Cabin Crew Scheduling Department', status_id: 1, code: 'CSD' },
+#   { description: 'Central Flight Dispatch', status_id: 1, code: 'CFD' },
+#   { description: 'Airport Terminal', status_id: 1, code: 'Airport Terminal' },
+#   { description: 'OCC', status_id: 1, code: 'OCC' },
+#   { description: 'SP', status_id: 1, code: 'SP' },
+# ])
 
 User.create!([
-  { email: 'mj_caabay@yahoo.com', password_digest: Digest::MD5.hexdigest('test000'), first_name: 'marc', last_name: 'caabay', user_department_id: 1, status_id: 1 },
+  # { email: 'mj_caabay@yahoo.com', password_digest: Digest::MD5.hexdigest('test000'), first_name: 'marc', last_name: 'caabay', user_department_id: 1, status_id: 1 },
   { email: 'test_ltp', password_digest: Digest::MD5.hexdigest('test000'), first_name: 'Test', last_name: 'LTP', user_department_id: 2, status_id: 1 },
   { email: 'test_fmd', password_digest: Digest::MD5.hexdigest('test000'), first_name: 'Test', last_name: 'FMD', user_department_id: 3, status_id: 1 },
   { email: 'test_rmd', password_digest: Digest::MD5.hexdigest('test000'), first_name: 'Test', last_name: 'RMD', user_department_id: 4, status_id: 1 },
@@ -27,10 +27,10 @@ User.create!([
   { email: 'test_sp', password_digest: Digest::MD5.hexdigest('test000'), first_name: 'Test', last_name: 'SP', user_department_id: 9, status_id: 1 },
 ])
 
-AircraftRegistry.create!([
-  { aircraft_name: 'RP-C7779', },
-  { aircraft_name: 'RP-C3501', },
-  { aircraft_name: 'RP-C3437', },
-  { aircraft_name: 'RP-C8784', },
-  { aircraft_name: 'RP-C9902', },
-])
+# AircraftRegistry.create!([
+#   { aircraft_name: 'RP-C7779', },
+#   { aircraft_name: 'RP-C3501', },
+#   { aircraft_name: 'RP-C3437', },
+#   { aircraft_name: 'RP-C8784', },
+#   { aircraft_name: 'RP-C9902', },
+# ])
